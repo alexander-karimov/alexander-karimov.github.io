393d3c6056d8d404d8fcc7319f87578552911b1a
KarimovAlexander <alexander.karimov94@yandex.ru>
2025-03-08 22:38:49 +0300

    Improved performance and update info

--- js/parallax-effect.js
+++ js/parallax-effect.js
@@ -1,8 +1,35 @@
-document.addEventListener('mousemove', e => {
-    Object.assign(document.documentElement, {
-        style: `
-      --move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
-      --move-y: ${(e.clientY - window.innerHeight / 2) * -.01}deg;
-    `
-    })
-})
+let targetX = 0,
+    targetY = 0;
+let velocityX = 0,
+    velocityY = 0;
+
+const speed = 0.1;
+const friction = 1;
+
+let centerX = window.innerWidth / 2;
+let centerY = window.innerHeight / 2;
+
+window.addEventListener("resize", updateCenter);
+function updateCenter() {
+    centerX = window.innerWidth / 2;
+    centerY = window.innerHeight / 2;
+}
+
+document.addEventListener("mousemove", onMouseMove, { passive: true });
+function onMouseMove(e) {
+    targetX = (e.clientX - centerX) * -0.005;
+    targetY = (e.clientY - centerY) * -0.01;
+}
+
+(function animate() {
+    velocityX += (targetX - velocityX) * speed;
+    velocityY += (targetY - velocityY) * speed;
+
+    velocityX *= friction;
+    velocityY *= friction;
+
+    document.documentElement.style.setProperty("--move-x", `${velocityX}deg`);
+    document.documentElement.style.setProperty("--move-y", `${velocityY}deg`);
+
+    requestAnimationFrame(animate);
+})();
